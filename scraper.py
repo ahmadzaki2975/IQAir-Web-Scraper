05eb4eeeaa36ce92b05d85871603cb3e1154df9f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-11-08 14:23:16 +0700

    fix: use strip() to remove whitespace

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # Base URL of IQAir website
 url = 'https://www.iqair.com/id/indonesia/central-java/salatiga'
@@ -22,7 +23,7 @@ if response.status_code == 200:
             pressure = td.text
         if ("km/h" in td.text):
             wind = td.text
-    time = soup.find('time')
+    time = soup.find('time').string.strip()
     icon_parent = soup.find('img', class_="forecast-wind_icon")
     wind_dir = icon_parent.get('alt')
     
@@ -34,8 +35,19 @@ if response.status_code == 200:
     print('Wind Direction:', wind_dir)
     print('Wind Speed:', wind)
     print('Wind Pressure:', pressure)
-    print('Last Update:', time.string)
+    print('Last Update:', time)
     print("=====================================")
+
+    # Write the extracted data to a CSV file
+    with open('airQualityData.csv', mode='w') as csv_file:
+        fieldnames = ['city', 'index', 'wind_dir', 'wind', 'pressure', 'time']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        try:
+            writer.writeheader()
+            writer.writerow({'index': index, 'city': city.string, 'wind_dir': wind_dir, 'wind': wind, 'pressure': pressure, 'time': time})
+        except:
+            print("Error writing to CSV file.")
+
 else:
     print('Failed to retrieve the webpage.')
 
